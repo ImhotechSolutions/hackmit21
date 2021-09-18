616ae58faffc136f6cebdd14ea9b5bf798bb658b
Akshay Bodla <akshaybodla21@gmail.com>
2021-09-18 14:21:24 -0500

    created templates for antenatal profile flow

---
+++ frontend/src/pages/Patients/PatientForms/GestationalProfile.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react'
+import { Grid, } from '@material-ui/core';
+import Controls from "../../../components/controls/Controls";
+import { useForm, Form } from '../../../components/useForm';
+import * as employeeService from "../../../services/employeeService";
+
+import { Formik } from "formik";
+import * as Yup from "yup"
+
+
+
+
+const GestationalProfile = () => {
+
+
+    return (<></>)
+}
+
+export default GestationalProfile;
