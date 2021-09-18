9ca5966627e4250ba1e614184381cb40f98e1ef1
James Zhang <jameszhanga@gmail.com>
2021-09-18 17:51:38 -0500

    Able to fetch data with Node backend

--- FHIR/server/index.js
+++ FHIR/server/index.js
@@ -1,8 +1,27 @@
 const express = require('express');
-
+const fhirClient = require('fhirclient');
+const axios = require('axios');
 const PORT = process.env.PORT || 3001;
-
 const app = express();
+require('dotenv').config();
+
+const config = {
+  method  : 'get',
+  url     : `${process.env.API_URL}/Procedure`,
+  headers : {
+    'x-api-key' : process.env.XAPIKEY
+  }
+};
+
+axios(config).then(({ data }) => {
+  const { entry: patientData } = data;
+  console.log(patientData);
+  return patientData;
+});
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from server!' });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
