ed08a1cde93412b2a158309f751c35d9769f7ea5
James Zhang <jameszhanga@gmail.com>
2021-09-19 01:04:38 -0500

    Able to rerturn procedure data when calling api

--- FHIR/api/procedure.route.js
+++ FHIR/api/procedure.route.js
@@ -1,7 +1,28 @@
 import express from 'express';
+import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const router = express.Router();
 
-router.route('/').get((req, res) => res.send('hello from procedures'));
+// Info needed to connect to FHIR
+const config = {
+  method  : 'get',
+  url     : `${process.env.API_URL}/Procedure`,
+  headers : {
+    'x-api-key' : process.env.XAPIKEY
+  }
+};
+
+// Temp function to get procedure data
+const getProcedure = () => {
+  return axios(config).then(({ data }) => {
+    const { entry: procedures } = data;
+    // console.log(procedures);
+    return procedures;
+  });
+};
+
+router.route('/').get(getProcedure);
 
 export default router;
--- FHIR/server.js
+++ FHIR/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import axios from 'axios';
 import cors from 'cors';
 import procedure from './api/procedure.route.js';
 
@@ -7,9 +8,29 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Info needed to connect to FHIR
+const config = {
+  method  : 'get',
+  url     : `${process.env.API_URL}/Procedure`,
+  headers : {
+    'x-api-key' : process.env.XAPIKEY
+  }
+};
+
+// Temp function to get procedure data
+const getProcedure = async () => {
+  return axios(config).then(res => {
+    const { entry: procedures } = res.data;
+    // console.log(procedures);
+    return procedures;
+  });
+};
+
 // create a GET route
-app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from server!' });
+// Return the procedure data
+app.get('/api', async (req, res) => {
+  const procedure = await getProcedure();
+  res.send(JSON.stringify(procedure));
 });
 
 app.get('/express_backend', (req, res) => {
