8df30e0b2c684278c0930590530b898efbdbb64f
James Zhang <jameszhanga@gmail.com>
2021-09-19 02:06:56 -0500

    Able to fetch procedure data from frontend

--- frontend/src/pages/DataDisplay/DataDisplay.js
+++ frontend/src/pages/DataDisplay/DataDisplay.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getAll } from '../../services/procedures';
 
 const DataDisplay = () => {
-  return <div>TBD...</div>;
+  const [ procedures, setProcedures ] = useState([]);
+
+  useEffect(() => {
+    retrieveProcedures();
+  }, []);
+
+  // Gets an array of all the procedures from the backend
+  const retrieveProcedures = () => {
+    getAll().then(response => {
+      console.log(response.data);
+      setProcedures(response.data);
+    });
+  };
+
+  // Displays a list of all the patients
+  return (
+    <div>
+      {procedures.map(({ resource }) => {
+        console.log(resource);
+        return <p>{resource.subject.reference}</p>;
+      })}
+    </div>
+  );
 };
 
 export default DataDisplay;
